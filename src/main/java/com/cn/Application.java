7399dc3d7d793850ea697a7e72a2013e48183e67
tbskybj <2311943591@qq.com>
2017-07-10 14:54:15 +0800

    ##

--- src/main/java/com/cn/Application.java
+++ src/main/java/com/cn/Application.java
@@ -2,7 +2,7 @@ package com.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//标注启动类
+//标注启动类wwww
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
